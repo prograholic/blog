a42de551bc39ff99165fb81b8d348a7cafdb02f8
Alexey Kutumov <alexey.kutumov@gmail.com>
2012-07-28 02:28:30 +0700

    Added inserter

--- data_io/data_io_inserter.h
+++ data_io/data_io_inserter.h
@@ -1,5 +1,68 @@
 #ifndef DATA_IO_INSERTER_H
 #define DATA_IO_INSERTER_H
 
+#include "data_io_endian.h"
+
+
+namespace dio
+{
+	namespace detail
+	{
+		template <typename TagT, typename TypeT>
+		struct number_inserter
+		{
+			TypeT mX;
+
+			number_inserter(TypeT x): mX(x)
+			{}
+
+		};
+	}
+
+	enum le_tag
+	{
+		le
+	};
+
+	enum be_tag
+	{
+		be
+	};
+
+
+	template <typename IntT>
+	detail::number_inserter<le_tag, IntT> insert(le_tag, IntT x)
+	{
+		return detail::number_inserter<le_tag, IntT>(x);
+	}
+
+	template <typename IntT>
+	detail::number_inserter<be_tag, IntT> insert(be_tag, IntT x)
+	{
+		return detail::number_inserter<be_tag, IntT>(x);
+	}
+
+
+	template <typename TypeT>
+	data_t & operator << (data_t & data, const detail::number_inserter<le_tag, TypeT> & inserter)
+	{
+		endian::le_type_2_mem(inserter.mX, std::back_inserter(data));
+
+		return data;
+	}
+
+	template <typename TypeT>
+	data_t & operator << (data_t & data, const detail::number_inserter<be_tag, TypeT> & inserter)
+	{
+		endian::be_type_2_mem(inserter.mX, std::back_inserter(data));
+
+		return data;
+	}
+
+
+
+}
+
+
 
 #endif /* DATA_IO_INSERTER_H */
--- data_io/tests/data_io_tests.cpp
+++ data_io/tests/data_io_tests.cpp
@@ -4,15 +4,19 @@
 
 
 #include "data_io_endian.h"
+#include "data_io_inserter.h"
+
 
 
 BOOST_AUTO_TEST_SUITE(endian)
+
+using namespace dio::endian;
 
 BOOST_AUTO_TEST_CASE(test_little_endian_to_int)
 {
 	char buff[] = {0x00, 0x40};
 
-	BOOST_CHECK_EQUAL(0x4000, dio::endian::le_mem_2_type<dio::uint16_t>(buff));
+	BOOST_CHECK_EQUAL(0x4000, le_mem_2_type<dio::uint16_t>(buff));
 
 }
 
@@ -21,7 +25,7 @@ BOOST_AUTO_TEST_CASE(test_big_endian_to_int)
 {
 	char buff[] = {0x12, 0x34, 0x56, 0x78};
 
-	BOOST_CHECK_EQUAL(0x12345678, dio::endian::be_mem_2_type<dio::uint32_t>(buff));
+	BOOST_CHECK_EQUAL(0x12345678, be_mem_2_type<dio::uint32_t>(buff));
 
 }
 
@@ -33,7 +37,7 @@ BOOST_AUTO_TEST_CASE(test_little_endian_from_int)
 
 	char tmp[sizeof(x)];
 
-	dio::endian::le_type_2_mem(x, tmp);
+	le_type_2_mem(x, tmp);
 
 
 	BOOST_CHECK_EQUAL_COLLECTIONS(tmp, tmp + sizeof(x), etalon, etalon + sizeof(x));
@@ -47,11 +51,34 @@ BOOST_AUTO_TEST_CASE(test_big_endian_from_int)
 
 	char tmp[sizeof(x)];
 
-	dio::endian::be_type_2_mem(x, tmp);
+	be_type_2_mem(x, tmp);
 
 
 	BOOST_CHECK_EQUAL_COLLECTIONS(tmp, tmp + sizeof(x), etalon, etalon + sizeof(x));
 
 }
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+
+BOOST_AUTO_TEST_SUITE(inserter)
+
+using namespace dio;
+
+
+BOOST_AUTO_TEST_CASE(test_little_endian_inserter)
+{
+	data_t x;
+
+	x << insert(le, 0x12345678);
+
+	BOOST_CHECK_EQUAL(0x78, x[0]);
+	BOOST_CHECK_EQUAL(0x56, x[1]);
+	BOOST_CHECK_EQUAL(0x34, x[2]);
+	BOOST_CHECK_EQUAL(0x12, x[3]);
+}
+
+
 
 BOOST_AUTO_TEST_SUITE_END()
