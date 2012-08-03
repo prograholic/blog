730aa64f985e4cac9531fc58391bf13c6f4ab28b
Alexey Kutumov <alexey.kutumov@gmail.com>
2012-08-03 13:58:56 +0700

    Removed unneeded  data

--- data_io/tests/tests_common.cpp
+++
@@ -1,3 +0,0 @@
-#include "endian_tests.hpp"
-#include "inserter_common_tests.hpp"
-#include "vector_inserter_tests.hpp"
--- data_io/tests/tests_common.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef BOOST_DIO_TESTS_COMMON_HPP
-#define BOOST_DIO_TESTS_COMMON_HPP
-
-#define BOOST_TEST_MODULE dio
-
-#include <boost/test/included/unit_test.hpp>
-
-#endif /* BOOST_DIO_TESTS_COMMON_HPP */
